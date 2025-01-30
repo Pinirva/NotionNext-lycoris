a3df4d6a6c4ab4b3f79034cbd83ebef77f3c5eb4
ipolkju <solidsportal@proton.me>
2025-01-31 00:35:00 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉',
+    '人应该循环在怎样的时间里, 今天星期几,季节轮回的节点是什么,我有时候觉得自己应该清醒的追求不快乐的事情,有时候又觉得自由更胜一筹,也许有新的故事值得期待,也许没有也许',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
