3026f37a685bc5d83c8b538273f7c4174b508aae
pinirva <53863654+Pinirva@users.noreply.github.com>
2025-04-02 03:33:48 +0800

    Update font.config.js

--- conf/font.config.js
+++ conf/font.config.js
@@ -11,7 +11,7 @@ module.exports = {
   FONT_URL: [
     // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
     'https://iosevka-webfonts.github.io/iosevka/iosevka.css',
-    'https://fontsapi.zeoseven.com/2/main/result.css',
+    'https://cdn.jsdelivr.net/gh/tingjieguo/lxgw-bright-webfont/style.css',
     'https://fonts.googleapis.com/css?family=Bitter&display=swap',
     'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap',
     'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
@@ -41,7 +41,7 @@ module.exports = {
   // 衬线字体 例如'"LXGW WenKai"'
   FONT_SERIF: [
     // '"LXGW WenKai"',
-    '"LXGW ZhenKai"',
+    '"LXGW Bright"',
     '"Times New Roman"',
     'Bitter',
     '"Noto Serif SC"',
