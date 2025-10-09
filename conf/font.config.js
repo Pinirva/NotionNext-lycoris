5d7a31d21c408ce0d3dbed874a928e95339620f5
pinirva <53863654+Pinirva@users.noreply.github.com>
2025-10-10 00:20:54 +0800

    Update font.config.js

--- conf/font.config.js
+++ conf/font.config.js
@@ -12,7 +12,8 @@ module.exports = {
     // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
     'https://fonts.googleapis.com/css?family=Bitter:300,400,700&display=swap',
     'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300;400;500;700&display=swap',
-    'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300;400;500;700&display=swap'
+    'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300;400;500;700&display=swap',
+    'https://raw.githubusercontent.com/TingjieGuo/lxgw-bright-webfont/refs/heads/main/style.css'
   ],
 
   // 字体优化配置
@@ -22,6 +23,7 @@ module.exports = {
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
     // '"LXGW WenKai"',
+    '"LXGW Bright Medium"',
     '"PingFang SC"',
     '-apple-system',
     'BlinkMacSystemFont',
