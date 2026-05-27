7bf7579970eb153adfcb5b472181c82c67979543
pinirva <53863654+Pinirva@users.noreply.github.com>
2026-05-27 19:45:51 +0800

    Add 'Bitter' and 'Noto Sans SC' to font config

--- conf/font.config.js
+++ conf/font.config.js
@@ -22,6 +22,7 @@ module.exports = {
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
     // '"LXGW WenKai"',
+    '"Noto Sans SC"',
     '"PingFang SC"',
     '-apple-system',
     'BlinkMacSystemFont',
@@ -30,7 +31,6 @@ module.exports = {
     '"Segoe UI Emoji"',
     '"Segoe UI Symbol"',
     '"Segoe UI"',
-    '"Noto Sans SC"',
     'HarmonyOS_Regular',
     '"Helvetica Neue"',
     'Helvetica',
@@ -42,8 +42,8 @@ module.exports = {
   // 衬线字体 例如'"LXGW WenKai"'
   FONT_SERIF: [
     // '"LXGW WenKai"',
-    'Bitter',
     '"Noto Serif SC"',
+    'Bitter',
     'SimSun',
     '"Times New Roman"',
     'Times',
